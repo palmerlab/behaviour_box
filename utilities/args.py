e7a72e7f8191042d146d5e72a4923fb3c70a5e07
amicallef <amicallef@student.unimelb.edu.au>
2016-06-11 19:15:16 +1000

    Makes args a dictionary

--- utilities/args.py
+++ utilities/args.py
@@ -4,161 +4,165 @@ p = argparse.ArgumentParser(description="This program controls the Arduino "
                                            "and reads from it too"
                                             )
 
-p.add_argument("-i", "--ID", 
-                default = "", 
-                help = "identifier for this animal/run",
-                )
+kwargs = {
+    ("-i", "--ID", ) : {
+                    'default' : "", 
+                    'help' : "identifier for this animal/run",
+                    },
 
-p.add_argument("-w", "--weight", 
-                default = 0, 
-                help = "weight of the animal in grams",
-                )
+    ("-w", "--weight", ) : {
+                    'default' : 0, 
+                    'help' : "weight of the animal in grams",
+                    },
 
-p.add_argument("-m", 
-                "--mode", 
-                default = "o", 
-                help = "the mode `h`abituaton or `o`perant, "
-                        "by default will look in the config table",
-                )           
-                
-p.add_argument('--repeats', 
-                default = 10, 
-                type = int, 
-                help = "the number of times this block should repeat, " 
-                        "by default this is 1",
-                )
-                
-p.add_argument('-p', '--punish', 
-                action = 'store_true', 
-                help = "sets `break_wrongChoice` to True, " 
-                        "incorrect licks will end an operant "
-                        "trial early",
-                )
-p.add_argument("--verbose", 
-                action = 'store_true', 
-                help = "for debug this will print everything if enabled",
-                )
+    ("-m", "--mode",) : { 
+                    'default' : "o", 
+                    'help' : "the mode `h`abituaton or `o`perant, "
+                            "by default will look in the config table",
+                    },          
+                    
+    ('--repeats', ) : {
+                    'default' : 10, 
+                    'type' : int, 
+                    'help' : "the number of times this block should repeat, " 
+                            "by default this is 1",
+                    },
+                    
+    ('-p', '--punish', ) : {
+                    'action' : 'store_true', 
+                    'help' : "sets `break_wrongChoice` to True, " 
+                            "incorrect licks will end an operant "
+                            "trial early",
+                    },
+    ("--verbose", ) : {
+                    'action' : 'store_true', 
+                    'help' : "for debug this will print everything if enabled",
+                    },
 
-p.add_argument("-a", "--auditory", 
-                action = 'store_true', 
-                help = "switch to auditory stimulus instead of somatosensory",
-                )
+    ("-a", "--auditory", ) : {
+                    'action' : 'store_true', 
+                    'help' : "switch to auditory stimulus instead of somatosensory",
+                    },
 
-p.add_argument("-bc", "--bias_correct", 
-                action = 'store_true', 
-                help = "turn on the bias correction for the random number generator",
-                )
-                
-p.add_argument("-b", "--blanks", 
-                action = 'store_true', 
-                help = "include no stim trials",
-                )
-                
-p.add_argument("-rs", "--right_same", 
-                action = 'store_true', 
-                help = "define the right port as correct for same stimulus",
-                )
-                
-p.add_argument("-s", "--single", 
-                action = 'store_true', 
-                help = "use this flag for a single stimulus only",
-                )
+    ("-bc", "--bias_correct", ) : {
+                    'action' : 'store_true', 
+                    'help' : "turn on the bias correction for the random number generator",
+                    },
+                    
+    ("-b", "--blanks", ) : {
+                    'action' : 'store_true', 
+                    'help' : "include no stim trials",
+                    },
+                    
+    ("-rs", "--right_same", ) : {
+                    'action' : 'store_true', 
+                    'help' : "define the right port as correct for same stimulus",
+                    },
+                    
+    ("-s", "--single", ) : {
+                    'action' : 'store_true', 
+                    'help' : "use this flag for a single stimulus only",
+                    },
 
-                
-p.add_argument('-lt', '--lickThres', 
-                default = 0.75, 
-                type = float, 
-                help = 'set `lickThres` in arduino',
-                )
-                
-p.add_argument('-lc', '--lcount', 
-                default = 2, 
-                type = int, 
-                help = 'set `minlickCount` in arduino'
-                )
+                    
+    ('-lt', '--lickThres', ) : {
+                    'default' : 0.75, 
+                    'type' : float, 
+                    'help' : 'set `lickThres` in arduino',
+                    },
+                    
+    ('-lc', '--lcount', ) : {
+                    'default' : 2, 
+                    'type' : int, 
+                    'help' : 'set `minlickCount` in arduino'
+                    },
 
-p.add_argument('-nlp', '--noLick', 
-                default = 1000, 
-                type = int, 
-                help = 'set `t_noLickPer` in arduino'
-                )
-                
-p.add_argument('-td', '--trialDur', 
-                default = 0,
-                type = float,
-                help = 'set minimum trial duration'
-                )             
+    ('-nlp', '--noLick', ) : {
+                    'default' : 1000, 
+                    'type' : int, 
+                    'help' : 'set `t_noLickPer` in arduino'
+                    },
+                    
+    ('-td', '--trialDur', ) : {
+                    'default' : 0,
+                    'type' : float,
+                    'help' : 'set minimum trial duration'
+                    },
+                    
+    ('-rd', '--t_rewardSTART', ) : {
+                    'default' : 3000, 
+                    'type' : int, 
+                    'help' : 'set start time of reward epoch'
+                    },
+                    
+    ('-rend', '--t_rewardEND', ) : {
+                    'default' : 4000, 
+                    'type' : int, 
+                    'help' : 'set end time of reward epoch'
+                    },
+                    
+    ('-to', '--timeout', ) : {
+                    'default' : 1.5,
+                    'type' : float,
+                    'help' : 'set the timeout duration for incorrect licks'
+                    },
 
-p.add_argument('-rd', '--t_rewardSTART', 
-                default = 3000, 
-                type = int, 
-                help = 'set start time of reward epoch'
-                )
-                
-p.add_argument('-rend', '--t_rewardEND', 
-                default = 4000, 
-                type = int, 
-                help = 'set end time of reward epoch'
-                )
-                
-p.add_argument('-to', '--timeout', 
-                default = 1.5,
-                type = float,
-                help = 'set the timeout duration for incorrect licks'
-                )
+    ('--freq', ) : {
+                    'nargs' : 2, 
+                    'default' : [0,200], 
+                    'type' : int, 
+                    'help' : "Frequencies or OFF time values to be passed to "
+                            "arduino as off_short and off_long",
+                    },
 
-p.add_argument('--freq', 
-                nargs = 2, 
-                default = [0,200], 
-                type = int, 
-                help = "Frequencies or OFF time values to be passed to "
-                        "arduino as off_short and off_long",
-                )
+    ('--dur', ) : {
+                    'nargs' : 2, 
+                    'default' : [100,500], 
+                    'type' : int, 
+                    'help' : "Durations or to be passed to "
+                            "arduino as DUR_short and DUR_long",
+                    },
 
-p.add_argument('--dur', 
-                nargs = 2, 
-                default = [100,500], 
-                type = int, 
-                help = "Durations or to be passed to "
-                        "arduino as DUR_short and DUR_long",
-                )
+    ('--ITI', ) : {
+                    'nargs' : 2, 
+                    'default' : [1,3], 
+                    'type' : float, 
+                    'help' : "an interval for randomising between trials",
+                    },
+                    
 
-p.add_argument('--ITI', 
-                nargs = 2, 
-                default = [1,3], 
-                type = float, 
-                help = "an interval for randomising between trials",
-                )
-                
+
+    ("-N", '--trial_num', ) : {
+                    'default' : 0, 
+                    'type' : int, 
+                    'help' : 'trial number to start at',
+                    },
+
+    ('--datapath', ) : {
+                    'default' : "C:/DATA/Andrew/wavesurfer", 
+                    'help' : "path to save data to, " 
+                            "by default is "
+                            "'C:/DATA/Andrew/wavesurfer/%%YY%%MM%%DD'",
+                    },
+                    
+    ("--port", ) : {
+                    'default' : "COM5", 
+                    'help' : "port that the Arduino is connected to",
+                    },
+}
+
+
+for k, v in kwargs.iteritems():
+    p.add_argument(*k, **v)
+
+
 lickside = p.add_mutually_exclusive_group()
-
-lickside.add_argument("-L", 
-                "--left", 
+lickside.add_argument("-L", "--left", 
                 action = 'store_true',
                 )
                 
-lickside.add_argument("-R", 
-                "--right", 
+lickside.add_argument("-R", "--right", 
                 action = 'store_true',
                 )
-
-p.add_argument("-N", '--trial_num', 
-                default = 0, 
-                type = int, 
-                help = 'trial number to start at',
-                )
-
-p.add_argument('--datapath', 
-                default = "C:/DATA/Andrew/wavesurfer", 
-                help = "path to save data to, " 
-                        "by default is "
-                        "'C:/DATA/Andrew/wavesurfer/%%YY%%MM%%DD'",
-                )
-                
-p.add_argument("--port", 
-                default = "COM5", 
-                help = "port that the Arduino is connected to",
-                )
-                
 
 args = p.parse_args()
