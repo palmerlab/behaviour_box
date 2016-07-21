ad14278286e4d871e8c705380708c2af94913862
andrew <amicallef@student.unimelb.edu.au>
2016-07-21 16:21:38 +1000

    unknown

--- behaviourbox/single_port_setup.h
+++ behaviourbox/single_port_setup.h
@@ -63,7 +63,8 @@ char runTrial() {
     t = t_since(t_init);
 
     //tone(speakerPin, toneGood, 50);
-    count = count_responses(t_rewardDUR, count, timeout, (trialType == 'N'));
+    count = count_responses(t_rewardDUR - t_stimDUR - t_rewardDEL, 
+            count, timeout, (trialType == 'N'));
 
     if (trialType == 'G') {
         if (count >= minlickCount) {
