a6b5caa34c91ca488cad47e083f8b4aa885567dc
andrew <amicallef@student.unimelb.edu.au>
2016-07-21 21:24:27 +1000

    experiment status

--- SerialControl.py
+++ SerialControl.py
@@ -523,7 +523,7 @@ try:
         for r in xrange(repeats):
             trials = ([100] * 20)
             #[trials.append(0) for i in range(2)]
-            [trials.append(600) for i in range(10)]
+            [trials.append(600) for i in range(5)]
            
             shuffle(trials)
             print trials
