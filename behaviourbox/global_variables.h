54ca3ed8433a2b9031f22bc0d112192df4186163
amicallef <amicallef@student.unimelb.edu.au>
2017-04-05 16:26:00 +1000

    remove deprecated reward_count

--- behaviourbox/global_variables.h
+++ behaviourbox/global_variables.h
@@ -11,7 +11,7 @@ const byte speakerPin = 7;        // reward / cue tone
 const byte statusLED = 13;        // led connected to digital pin 13
 const byte lickSens = A0;
 
-const char waterPort = 10; // digital pins 10, 11 control water valve 
+const char waterPort = 10; // digital pins 10, 11 control water valve
 
 const String end_trial_msg = "- Status: Ready";
 
@@ -36,14 +36,12 @@ unsigned int t_rewardDUR = 2000;  // ms
 unsigned int t_trialDUR = 5000;  // ms
 unsigned int timeout = 0;         // ms
 
-byte debounce = 5;         // a simple debounce method 
+byte debounce = 5;         // a simple debounce method
 
 char mode = '-';                  //one of 'h'abituation, 'o'perant
 byte minlickCount = 5;
 bool lickTrigReward = 1;
 bool reward_nogo = 0;
-
-byte reward_count = 0;     // Globals to count number of continuous left and rights
 
 // stimulus parameters
 // -------------------
@@ -53,7 +51,7 @@ byte reward_count = 0;     // Globals to count number of continuous left and rig
 // ------
 
 // Global value to keep track of the total water consumed
-// this really shouldn't get much higher than 100. 
+// this really shouldn't get much higher than 100.
 
 byte waterVol = 10;               // uL per dispense
 char trialType = 'G';             // 'G' or 'N'
