3f6a554982e9a0c01744afb05fde1ec7cde6871b
lab <lab@HFLD-03574-W.unimelb.edu.au>
2017-11-10 16:03:12 +1100

    updated args to run on THIS computer

--- utilities/args.py
+++ utilities/args.py
@@ -1,60 +1,60 @@
 import argparse
 
-p = argparse.ArgumentParser(description="This program controls the Arduino " 
+p = argparse.ArgumentParser(description="This program controls the Arduino "
                                            "and reads from it too"
                                             )
 
 kwargs = {
     ("-i", "--ID", ) : {
-                    'default' : "", 
+                    'default' : "",
                     'help' : "identifier for this animal/run",
                     },
 
     ("-w", "--weight", ) : {
-                    'default' : 0, 
+                    'default' : 0,
                     'help' : "weight of the animal in grams",
                     },
 
-    ("-m", "--mode",) : { 
-                    'default' : "o", 
+    ("-m", "--mode",) : {
+                    'default' : "o",
                     'help' : "the mode `h`abituaton or `o`perant, "
                             "by default will look in the config table",
-                    },          
-                    
+                    },
+
     ('--repeats', ) : {
-                    'default' : 500, 
-                    'type' : int, 
-                    'help' : "the number of times this block should repeat, " 
+                    'default' : 500,
+                    'type' : int,
+                    'help' : "the number of times this block should repeat, "
                             "by default this is 1",
                     },
-                    
+
     ('-p', '--punish', ) : {
-                    'action' : 'store_false', 
-                    'help' : "sets `break_wrongChoice` to True, " 
+                    'action' : 'store_false',
+                    'help' : "sets `break_wrongChoice` to True, "
                             "incorrect licks will end an operant "
                             "trial early",
                     },
 
     ("--verbose", ) : {
-                    'action' : 'store_true', 
+                    'action' : 'store_true',
                     'help' : "for debug this will print everything if enabled",
                     },
 
     ('-lt', '--lickThres', ) : {
-                    'default' : 2, 
-                    'type' : float, 
+                    'default' : 2,
+                    'type' : float,
                     'help' : 'set `lickThres` in arduino',
                     },
-                    
+
     ('-lc', '--lcount', ) : {
-                    'default' : 2, 
-                    'type' : int, 
+                    'default' : 2,
+                    'type' : int,
                     'help' : 'set `minlickCount` in arduino'
                     },
 
     ('-nlp', '--noLick', ) : {
-                    'default' : 1500, 
-                    'type' : int, 
+                    'default' : 1500,
+                    'type' : int,
                     'help' : 'set `t_noLickPer` in arduino'
                     },
 
@@ -63,81 +63,81 @@ kwargs = {
                     'type' : float,
                     'help' : 'set minimum trial duration'
                     },
-                    
+
     ('--t_stimONSET',) : {
-                    'default' : 2000, 
-                    'type' : int, 
+                    'default' : 2000,
+                    'type' : int,
                     'help' : 'sets the time after trigger to run the first stimulus'
                     },
 
     ('-rdel', '--t_rDELAY', ) : {
-                    'default' : 50, 
-                    'type' : int, 
+                    'default' : 50,
+                    'type' : int,
                     'help' : 'set start time of reward epoch'
                     },
-                    
+
     ('-rdur', '--t_rDUR', ) : {
-                    'default' : 800, 
-                    'type' : int, 
+                    'default' : 800,
+                    'type' : int,
                     'help' : 'set end time of reward epoch'
                     },
-                    
+
     ('-to', '--timeout', ) : {
                     'default' : 2.5,
                     'type' : float,
                     'help' : 'set the timeout duration for incorrect licks'
                     },
-                    
+
     ('-ltr', '--lickTrigReward', ) : {
-                    'action' : 'store_true', 
+                    'action' : 'store_true',
                     'help' : 'flag to allow licks to trigger the reward immediatly'
                     },
-                    
+
     ('-rng', '--reward_nogo', ) : {
-                    'action' : 'store_true', 
+                    'action' : 'store_true',
                     'help' : 'flag to allow a water delivery following no lick of a no go stim'
-                    },    
+                    },
     ('-noise', ) : {
-                    'action' : 'store_true', 
+                    'action' : 'store_true',
                     'help' : 'plays a noise during trials'
                     },
 
     ('--ITI', ) : {
-                    'nargs' : 2, 
-                    'default' : [5,8], 
-                    'type' : float, 
+                    'nargs' : 2,
+                    'default' : [5,8],
+                    'type' : float,
                     'help' : "an interval for randomising between trials",
-                    },    
-                    
+                    },
+
     ('--trials', ) : {
-                    'nargs' : '*', 
-                    'default' : [200,0], 
-                    'type' : int, 
+                    'nargs' : '*',
+                    'default' : [200,0],
+                    'type' : int,
                     'help' : "durations to run on each trial",
-                    },    
-    
+                    },
+
     ('-ratio', ) : {
-                    'nargs' : 3, 
-                    'default' : [1,1,0], 
-                    'type' : int, 
+                    'nargs' : 3,
+                    'default' : [1,1,0],
+                    'type' : int,
                     'help' : "number of go, nogo, and blank trials respectively",
                     },
     ('-restore', ) : {
                      'action': 'store_true',
                      'help' : "Use to look up previous settings in the comms.ini file",
                     },
-    
+
     ("-N", '--trial_num', ) : {
-                    'default' : 0, 
-                    'type' : int, 
+                    'default' : 0,
+                    'type' : int,
                     'help' : 'trial number to start at',
                     },
 
     ('--datapath', ) : {
-                    'default' : r"R:\Andrew\170426_GOnoGO_Perception_APV", 
-                    'help' : "path to save data to, " 
+                    'default' : r"./data",
+                    'help' : "path to save data to, "
                             "by default is "
-                            r"R:\Andrew\170426_GOnoGO_Perception_APV\%%YY%%MM%%DD",
+                            "./data",
                     },
     ('-af', '--audio') :{
                     'action' : 'store_true',
@@ -145,9 +145,9 @@ kwargs = {
                                " this is not to be confused with the noise"
                                " played to simulate / mask the scanners"
                     },
-                    
+
     ("--port", ) : {
-                    'default' : "COM5", 
+                    'default' : "COM6",
                     'help' : "port that the Arduino is connected to",
                     },
 }
