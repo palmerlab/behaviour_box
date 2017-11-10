0f26626929bdf8a039138738517f93c71621f22f
lab <lab@HFLD-03574-W.unimelb.edu.au>
2017-11-10 14:51:54 +1100

    deleted redundant variable

--- behaviourbox/SerialComms.h
+++ behaviourbox/SerialComms.h
@@ -5,24 +5,24 @@
 String getSerialInput(){
 
     /*
-      This function reads the data from the serial 
-      connection and returns it as a string. 
-      
+      This function reads the data from the serial
+      connection and returns it as a string.
+
       This is used later to update the values
     */
     String readString;
-    
-    while (Serial.available()) { 
+
+    while (Serial.available()) {
         /* 1. delay to allow buffer to fill
            2. get one byte from serial buffer
-           3. make the string readString 
+           3. make the string readString
        */
-        
-        delay(3);  
-        char c = Serial.read();  
-        readString += c; 
+
+        delay(3);
+        char c = Serial.read();
+        readString += c;
     }
-    
+
     return readString;
 }
 
@@ -31,14 +31,14 @@ int getSepIndex(String input, char separator) {
       Returns the index of the separator character
       in a string.
     */
-    
+
     char c = 1;
     int i = 0;
-   
+
     while (c != 0) {
         c = input[i];
-        if (c == separator){ 
-            return i; 
+        if (c == separator){
+            return i;
         }
         i ++;
     }
@@ -49,9 +49,9 @@ int UpdateGlobals(String input) {
     /*
     This is a big ugly function which compares the
     input string to the names of variables that I have
-    stored in memory; This is very much not the `C` 
+    stored in memory; This is very much not the `C`
     way to do things...
-    
+
     I think this could be a hash table. I haven't
     learned enough about hash table implementation
     yet and I know this works, so for the moment:
@@ -62,15 +62,15 @@ int UpdateGlobals(String input) {
     int sep = getSepIndex(input, ':');
 
     if (sep) {
-        
+
         String variable_name = input.substring(0,sep);
         String variable_value = input.substring(sep+1);
-        
+
         Serial.print("\t#");
         Serial.print(variable_name);
         Serial.print(":");
         Serial.println(variable_value);
-        
+
         // input before seperator?
         // lickThres
         if (variable_name == "lickThres") {
@@ -122,7 +122,7 @@ int UpdateGlobals(String input) {
                 Serial.println(timeout);
                 return 1;
         }
-        
+
         // reward_nogo
         else if (variable_name == "reward_nogo") {
                 reward_nogo = bool(variable_value.toInt());
@@ -130,7 +130,7 @@ int UpdateGlobals(String input) {
                 Serial.println(reward_nogo);
                 return 1;
         }
-        
+
         // lickTrigReward
         else if (variable_name == "lickTrigReward") {
                 lickTrigReward = bool(variable_value.toInt());
@@ -138,7 +138,7 @@ int UpdateGlobals(String input) {
                 Serial.println(lickTrigReward);
                 return 1;
         }
-        
+
         // t_stimONSET
         else if (variable_name == "t_stimONSET") {
                 t_stimONSET = variable_value.toInt();
@@ -170,7 +170,7 @@ int UpdateGlobals(String input) {
                 Serial.println(t_rewardDUR);
                 return 1;
         }
-        
+
         // t_trialDUR
         else if (variable_name == "t_trialDUR") {
                 t_trialDUR = variable_value.toInt();
@@ -187,14 +187,6 @@ int UpdateGlobals(String input) {
                 return 1;
         }
 
-        // Nports
-        else if (variable_name == "Nports") {
-                Nports = variable_value.toInt();
-                Serial.print("\tNports:");
-                Serial.println(Nports);
-                return 1;
-        }
-        
         // punish_tone
         else if (variable_name == "punish_tone") {
                 punish_tone = variable_value.toInt();
@@ -202,7 +194,7 @@ int UpdateGlobals(String input) {
                 Serial.println(punish_tone);
                 return 1;
         }
-        
+
         // debounce
         else if (variable_name == "debounce") {
                 debounce = variable_value.toInt();
@@ -210,7 +202,7 @@ int UpdateGlobals(String input) {
                 Serial.println(debounce);
                 return 1;
         }
-        
+
         // audio
         else if (variable_name == "audio") {
                 audio = variable_value.toInt();
@@ -221,5 +213,3 @@ int UpdateGlobals(String input) {
    }
    return 0;
 }
-
-
