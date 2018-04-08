9905b01fe5f3a612cccfc64743e5ce0c2ac46ca2
lab <lab@HFLD-03574-W.unimelb.edu.au>
2018-04-09 09:31:48 +1000

    locks order of arguments by making the definition a list of dicts

--- utilities/args.py
+++ utilities/args.py
@@ -4,74 +4,75 @@ p = argparse.ArgumentParser(description="This program controls the Arduino "
                                            "and reads from it too"
                                             )
 
-kwargs = {
-    ("-i", "--ID", ) : {
+kwargs = [
+    {("-i", "--ID", ) : {
                     'default' : "",
                     'help' : "identifier for this animal/run",
-                    },
+                    }},
 
-    ('-r', '--repeats', ) : {
+    {('-r', '--repeats', ) : {
                     'default' : 5,
                     'type' : int,
                     'help' : "the number of times this block should repeat, "
                             "by default this is 1",
-                    },
+                    }},
 
-    ('-noise', ) : {
+    {('-noise', ) : {
                     'action' : 'store_true',
                     'help' : 'plays a noise during trials'
-                    },
+                    }},
 
-    ('-ITI', ) : {
+    {('-ITI', ) : {
                     'nargs' : 2,
                     'default' : [2,5],
                     'type' : float,
                     'help' : "an interval for randomising between trials",
-                    },
+                    }},
 
-    ('-datapath', ) : {
+    {('-datapath', ) : {
                     'default' : "./data",
                     'help' : "path to save data to, "
                             "by default is "
                             "./data",
-                    },
+                    }},
 
-    ('-fname', ) : {
+    {('-fname', ) : {
                     'default' : "",
                     'help' : "name of file "
                             "if left blank defaults to "
                             "ID + todays date as ID_YYMMDD"
-                    },
+                    }},
 
-    ("-port", ) : {
+    {("-port", ) : {
                     'default' : "COM7",
                     'help' : "port that the Arduino is connected to",
-                    },
-    ("-m", "--mode", ) : {
+                    }},
+    {("-m", "--mode", ) : {
                     'default' : "o",
                     'help' : "mode of trial to run",
-                    },
-    ('-Stim', '-s',) : {
+                    }},
+    {('-Stim', '-s',) : {
                     'nargs' : '+',
                     'default' : (1,),
                     'type' : int,
                     'help' : "",
-                    },
-    ('-Light_resp', '-lr') : {
+                    }},
+    {('-Light_resp', '-lr') : {
                     'nargs' : '+',
                     'default' : (0,),
                     'type' : int,
                     'help' : "",
-                    },
-    ('-Light', '-l') : {
+                    }},
+    {('-Light', '-l') : {
                     'nargs' : '+',
                     'default' : (0,),
                     'type' : int,
                     'help' : "",
-                    },
-}
+                    }},
+]
 
-for k, v in kwargs.iteritems():
+for argument in kwargs:
+    (k,v), = argument.items()
     p.add_argument(*k, **v)
 
 args = p.parse_args()
